2e2138fc853218ff829ff8647df63f483ebb5d06
HoangMinhTuan <21130600@st.hcmuaf.edu.vn>
2023-11-25 12:01:13 +0700

    push code

---
+++ Nhom55_WebBanTraiCay/src/main/java/nhom55/hcmuaf/controller/Home.java
@@ -0,0 +1,38 @@
+package nhom55.hcmuaf.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = { "/" })
+public class Home extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public Home() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Forward to /WEB-INF/views/homeView.jsp
+        // Dẫn đến đường link trang chủ
+        // (Users can not access directly into JSP pages placed in WEB-INF)
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp");
+
+        dispatcher.forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
+
--- Nhom55_WebBanTraiCay/src/main/java/nhom55/hcmuaf/controller/Login.java
+++ Nhom55_WebBanTraiCay/src/main/java/nhom55/hcmuaf/controller/Login.java
@@ -1,26 +1,52 @@
 package nhom55.hcmuaf.controller;
 
+import nhom55.hcmuaf.beans.Users;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-@WebServlet(name = "login", value = "/login")
+@WebServlet(name = "Login", value = "/Login")
 public class Login extends HttpServlet {
-    public Login() {
-        super();
-    }
-    // Show Login page.
+
+
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = this.getServletContext ().getRequestDispatcher ("/WEB-INF/login/login.jsp");
         dispatcher.forward (request,response);
     }
 
-    // When the user enters userName & password, and click Submit.
-    // This method will be executed.
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
 
+
+
+        List<Users> listUsers = new ArrayList<>();
+        Users user1 = new Users("minhtuan123@gmail.com" , "12345", 1);
+        Users user2 = new Users("minhtu123@gmail.com" , "12345", 1);
+        Users user3 = new Users();
+
+        listUsers.add(user1);
+        listUsers.add(user2);
+        listUsers.add(user3);
+        for (Users  u : listUsers) {
+            if (u.getEmail().equals(email) && u.getPassword().equals(password)) {
+                request.setAttribute("user", email);
+                RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp");
+                dispatcher.forward(request, response);
+            }
+        }
+        request.setAttribute("user", email);
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/login/login.jsp");
+        dispatcher.forward(request, response);
     }
 }

17c449effe18849819b922bea8e0aa02849cc82f
giaosukirito <21130612@st.hcmuaf.edu.vn>
2023-12-02 09:32:37 +0700

    push cart

---
+++ Nhom55_WebBanTraiCay/src/main/java/nhom55/hcmuaf/controller/Cart.java
@@ -0,0 +1,22 @@
+package nhom55.hcmuaf.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "cart", value = "/cart")
+public class Cart extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+  }
+}
